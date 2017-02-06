5a42854148a44246bd2a18616b897a1174054276
gilead18-meet <gileadbalshayi@gmail.com>
2017-02-06 16:57:49 +0200

    hi

---
+++ test.py
@@ -0,0 +1,29 @@
+from turtle_chat_client import Client
+import turtle
+from turtle_chat_widgets import Button , TextInput
+class TextBox(TextInput):
+    def draw_box(self):
+        d_t_b=turtle.clone() #d_t_b = drawing the box
+        d_t_b.penup()
+        d_t_b.goto(self.width, self.height)
+        d_t_b.pendown()
+        d_t_b.goto(self.width, 0)
+        d_t_b.goto(0, 0)
+        d_t_b.goto(0, self.height)
+        d_t_b.goto(self.width, self.height)
+    def write_msg(self):
+        self.setup_listeners()
+        print(self.new_msg)
+        self.writer.goto(10, self.height - 15)
+        self.writer.clear()
+        self.writer.write(self.new_msg)
+        
+        
+
+db = TextBox()
+
+
+
+
+
+        
--- turtle_chat_view.py
+++ turtle_chat_view.py
@@ -5,6 +5,30 @@
 #                                   IMPORTS                                         #
 #####################################################################################
 
+##from turtle_chat_client import Client
+##import turtle
+##from turtle_chat_widgets import Button , TextInput
+##class TextBox(TextInput):
+##    def draw_box(self):
+##        d_t_b=turtle.clone() #d_t_b = drawing the box
+##        d_t_b.penup()
+##        d_t_b.goto(self.width, self.height)
+##        d_t_b.pendown()
+##        d_t_b.goto(self.width, 0)
+##        d_t_b.goto(0, 0)
+##        d_t_b.goto(0, self.height)
+##        d_t_b.goto(self.width, self.height)
+##    def write_msg(self):
+##        
+##        self.writer.write("hi")
+##        self.writer.clear("hi")
+##        
+##        
+##        
+##        
+##
+##TextBox()
+
 from turtle_chat_client import Client
 import turtle
 from turtle_chat_widgets import Button , TextInput
@@ -19,12 +43,15 @@ class TextBox(TextInput):
         d_t_b.goto(0, self.height)
         d_t_b.goto(self.width, self.height)
     def write_msg(self):
-        
-        self.writer.write("hi")
+        self.setup_listeners()
+        print(self.new_msg)
+        self.writer.goto(10, self.height - 15)
+        self.writer.clear()
+        self.writer.write(self.new_msg)
         
         
 
-TextBox()
+db = TextBox()
         
 
 #import the turtle module
